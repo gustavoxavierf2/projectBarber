ce384bf85018db82c836d19a579d20dc60780ea1
gustavo xavier <guxfarias7@gmail.com>
2025-04-18 19:02:21 -0300

    initial commit

---
+++ src/test/java/project/barber/barberShop/BarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package project.barber.barberShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
